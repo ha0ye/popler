f52995c9c7649f91ad36fe006730bbeece624e17
Hao Ye <hao.ye@weecology.org>
2019-05-21 15:14:34 -0400

    initial commit of scripts and Rmd template
    Co-authored-by: Ellen Bledsoe <ellen.bledsoe@ufl.edu>

---
+++ summarize_data_organization_hierarchies.R
@@ -0,0 +1,29 @@
+all_data <- readRDS("list_df.rds")
+
+for (i in seq(length(all_data)))
+{
+  rmarkdown::render(
+    "popler_data_report.Rmd", params = list(
+      dataset_index = i),
+    output_file = paste0("data_report-", i, ".md")
+  )
+}
+
+
+
+
+
+
+
+# x %>%
+#   select(year, month, day, 
+#          sppcode, 
+#          starts_with("spatial_replication_level"), 
+#          count_observation) %>%
+#   mutate(date = lubridate::ymd(paste(year, month, day, sep = "-"))) %>%
+#   mutate_at(vars("sppcode", starts_with("spatial_replication_level")), as.factor) %>%
+#   select(-year, -month, -day) %>%
+#   {.} -> xx
+# 
+# xx[, c("spatial_replication_level_3", "spatial_replication_level_4")] %>% table()
+
